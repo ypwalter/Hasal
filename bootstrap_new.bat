630656a8de25674e168ea3c945d9bca3ac154ae6
Walter Chen <ypwalter@gmail.com>
2016-11-03 14:05:29 +0800

    testing of new bootstrap

---
+++ bootstrap_new.bat
@@ -0,0 +1,194 @@
+@REM Author: Walter Chen
+@REM Version: 0.1.0
+@REM 0.1.0 - Successfully running in virtual machines, appveyor, and real machines.
+
+@REM Assuming that you already git pull all the files, we can use curl from the repository
+@REM Print out the time we started this script.
+@echo off
+for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
+set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
+echo [INFO] Current date and time [%ldt%]
+
+SET PATH=%MINICONDA%;%MINICONDA%\Scripts;%PATH%
+
+::::::::::::::::::::
+::  Prerequisite  ::
+::::::::::::::::::::
+
+@REM Checking Administrator Privilege
+
+@REM If in appveyor, skip download and installation.
+IF "%APPVEYOR%"=="True" goto NoAdmin_CI
+
+AT > NUL
+IF %ERRORLEVEL% EQU 0 (
+    ECHO [INFO] You are Administrator and able to run this script.
+) ELSE (
+    ECHO [FAIL] You are NOT Administrator. Please rerun with Administrator privilege.
+    PING 127.0.0.1 > NUL 2>&1
+    EXIT /B 1
+)
+
+:NoAdmin_CI
+IF "%APPVEYOR%"=="True" (
+    ECHO [INFO] Skipping checking of Administrator privilege in CI
+)
+
+
+@REM Checking Java
+ECHO [INFO] Checking Java
+
+@REM If in appveyor, skip download and installation.
+IF "%APPVEYOR%"=="True" goto Java_CI
+
+FOR /f %%j IN ("java.exe") DO (
+    SET JAVA_HOME=%%~dp$PATH:j
+)
+
+IF %JAVA_HOME%.==. (
+    ECHO [INFO] Downloading Java JDK 7u79.
+    thirdParty\curl -L -O -H "Cookie:oraclelicense=accept-securebackup-cookie" -k "http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-windows-i586.exe"
+    ECHO [INFO] Installing Java JDK 7u79.
+    jdk-7u79-windows-i586.exe /s
+) ELSE (
+    ECHO [INFO] Java JDK exists in the environment.
+    ECHO JAVA_HOME = %JAVA_HOME%
+)
+
+:Java_CI
+IF "%APPVEYOR%"=="True" (
+    ECHO [INFO] Skipping checking of Java in CI
+)
+
+
+::::::::::::::::::::
+::  Installation  ::
+::::::::::::::::::::
+
+@REM Checking and Installing 7zip
+ECHO [INFO] Checking 7zip
+
+@REM If in appveyor, skip download and installation.
+IF "%APPVEYOR%"=="True" goto 7zip_CI
+
+@REM Trying to download and install 7zip
+where 7z.exe >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO [INFO] You already have 7Zip in windows system.
+) ELSE (
+    IF EXIST 7z1604.exe (
+        ECHO [INFO] Found cached 7z1604.exe
+    ) ELSE (
+        ECHO [INFO] Downloading 7Zip.
+        thirdParty\curl -kLO http://www.7-zip.org/a/7z1604.exe
+        ECHO [INFO] Installing 7Zip.
+        7z1604.exe /S
+        SET "PATH=C:\Program Files\7-Zip;C:\Program Files (x86)\7-Zip;%PATH%"    
+    )
+)
+
+:7zip_CI
+IF "%APPVEYOR%"=="True" (
+    ECHO [INFO] Skipping checking of 7zip in CI
+    SET "PATH=C:\Program Files\7-Zip;%PATH%"
+)
+
+
+@REM Installing ffmpeg
+
+ECHO [INFO] Downloading FFMPEG.
+IF EXIST ffmpeg-20160527-git-d970f7b-win32-static.7z (
+    ECHO [INFO] Found cached ffmpeg-20160527-git-d970f7b-win32-static.7z
+) ELSE (
+    ECHO [INFO] Downloading FFMPEG.
+    thirdParty\curl -kLO https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-20160527-git-d970f7b-win32-static.7z
+)
+ECHO [INFO] Installing FFMPEG.
+7z x ffmpeg-20160527-git-d970f7b-win32-static.7z
+move /Y ffmpeg-20160527-git-d970f7b-win32-static ffmpeg
+SET PATH=%CD%\ffmpeg\bin\;%PATH%
+
+
+@REM Installing Sikuli
+
+IF EXIST sikulixsetup-1.1.0.jar (
+    ECHO [INFO] Found cached sikulixsetup-1.1.0.jar
+) ELSE (
+    ECHO [INFO] Downloading SikuliX 1.1.0
+    thirdParty\curl -kLO https://launchpad.net/sikuli/sikulix/1.1.0/+download/sikulixsetup-1.1.0.jar
+)
+ECHO [INFO] Installing SikuliX 1.1.0
+java -jar sikulixsetup-1.1.0.jar options 1.1 2
+copy runsikuli* thirdParty\
+copy sikuli*.jar thirdParty\
+@echo on
+
+
+@REM Installing Miniconda
+
+IF "%APPVEYOR%"=="True" GOTO SkipConda
+
+where conda.exe >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO [INFO] You already have conda in windows system.
+) ELSE (
+    ECHO [INFO] Downloading Miniconda.
+    thirdParty\curl -kLO https://repo.continuum.io/miniconda/Miniconda2-latest-Windows-x86.exe
+    ECHO [INFO] Installing Miniconda.
+    Miniconda2-latest-Windows-x86.exe /InstallationType=JustMe /RegisterPython=0 /S /D=C:\Miniconda2\
+    SET "PATH=C:\Miniconda2\Scripts\;C:\Miniconda2\;%PATH%"
+)
+
+:SkipConda
+IF "%APPVEYOR%"=="True" (
+    ECHO [INFO] Skipping checking of conda in CI
+)
+
+@REM Configuring Miniconda and Virtualenv
+conda config --set always_yes yes --set changeps1 no
+conda install psutil
+ECHO [INFO] Creating Miniconda virtualenv (It might take some time to finish.)
+conda create -q -n hasal-env python=2.7 numpy scipy nose pywin32 pip
+
+::::::::::::::::::::
+::    Browsers    ::
+::::::::::::::::::::
+
+@REM If in appveyor, skip download and installation
+IF NOT "%APPVEYOR%"=="True" (
+    @REM Installing firefox
+    ECHO [INFO] Downloading Firefox.
+    thirdParty\curl -kLO https://ftp.mozilla.org/pub/firefox/releases/49.0.1/win32/zh-TW/Firefox%%20Setup%%2049.0.1.exe
+    ECHO [INFO] Installing Firefox.
+    "Firefox%%20Setup%%2049.0.1.exe" -ms -ma
+    SET "PATH=C:\Program Files\Mozilla Firefox;C:\Program Files (x86)\Mozilla Firefox;%PATH%"
+
+    @REM Installing chrome
+    ECHO [INFO] Downloading Chrome.
+    thirdParty\curl -kLO http://dl.google.com/chrome/install/googlechromestandaloneenterprise.msi
+    ECHO [INFO] Installing Chrome.
+    msiexec /i "googlechromestandaloneenterprise.msi" /qn /quiet /norestart
+    SET "PATH=C:\Program Files\Google\Chrome\Application\;C:\Program Files (x86)\Google\Chrome\Application\;%PATH%"
+)
+
+::::::::::::::::::::
+::  Hasal  Setup  ::
+::::::::::::::::::::
+
+IF "%APPVEYOR%"=="True" (
+    ECHO [INFO] Setup in virtualenv
+    activate hasal-env
+    pip install coverage mitmproxy
+    pip install thirdParty\opencv_python-2.4.13-cp27-cp27m-win32.whl
+    python setup.py install
+) ELSE (
+    @REM Installing mitmproxy & opencv2 & Hasal
+    scripts\activate.bat & pip install mitmproxy thirdParty\opencv_python-2.4.13-cp27-cp27m-win32.whl & certutil -p "" thirdParty\mitmproxy-ca-cert.p12 & python setup.py install & python scripts\cv2_checker.py
+)
+
+::::::::::::::::::::
+::    Finished    ::
+::::::::::::::::::::
+
+echo "Please download certificates for Hasal from google drive or ask whoever know about it."
+@REM Bootstrap done
